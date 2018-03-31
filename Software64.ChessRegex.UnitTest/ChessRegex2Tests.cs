cd0c3e4009b408d1a3b1eb3e329a42262f8d1489
osotorrio <osotorrio@hotmail.com>
2018-03-31 17:02:07 +0100

    Rook moves tests Level 2

--- Software64.ChessRegex.UnitTest/ChessRegex2Tests.cs
+++ Software64.ChessRegex.UnitTest/ChessRegex2Tests.cs
@@ -10,17 +10,17 @@ namespace Software64.ChessRegex.UnitTest
     public class ChessRegex2Tests
     {
         [Test]
-        public void Pawn_standard_move_test()
+        public void Pawn_move_test()
         {
             // Arrange
             var expected = new List<string>
             {
                 "e4", "e4!", "e4!!", "e4?", "e4??", "e4!?", "e4?!",
                 "e4+", "e4+!", "e4+!!", "e4+?", "e4+??", "e4+!?", "e4+?!",
-                "e4#"
+                "e4++", "e4#",
             };
 
-            ActAndAssert(ChessRegex2.PawnStandard, expected);
+            ActAndAssert(ChessRegex2.PawnMoves, expected);
         }
 
         [Test]
@@ -31,7 +31,7 @@ namespace Software64.ChessRegex.UnitTest
             {
                 "e8=Q", "e8=Q!", "e8=Q!!", "e8=Q?", "e8=Q??", "e8=Q!?", "e8=Q?!",
                 "e8=Q+", "e8=Q+!", "e8=Q+!!", "e8=Q+?", "e8=Q+??", "e8=Q+!?", "e8=Q+?!",
-                "e8=Q#",
+                "e8=Q++", "e8=Q#",
             };
 
             ActAndAssert(ChessRegex2.PawnPromotes, expected);
@@ -45,7 +45,7 @@ namespace Software64.ChessRegex.UnitTest
             {
                 "dxe4", "dxe4!", "dxe4!!", "dxe4?", "dxe4??", "dxe4!?", "dxe4?!",
                 "dxe4+", "dxe4+!", "dxe4+!!", "dxe4+?", "dxe4+??", "dxe4+!?", "dxe4+?!",
-                "dxe4#",
+                "dxe4++", "dxe4#",
             };
 
             ActAndAssert(ChessRegex2.PawnCaptures, expected);
@@ -59,10 +59,94 @@ namespace Software64.ChessRegex.UnitTest
             {
                 "dxe8=Q", "dxe8=Q!", "dxe8=Q!!", "dxe8=Q?", "dxe8=Q??", "dxe8=Q!?", "dxe8=Q?!",
                 "dxe8=Q+", "dxe8=Q+!", "dxe8=Q+!!", "dxe8=Q+?", "dxe8=Q+??", "dxe8=Q+!?", "dxe8=Q+?!",
-                "dxe8=Q#",
+                "dxe8=Q++", "dxe8=Q#",
             };
 
             ActAndAssert(ChessRegex2.PawnCapturesPromotes, expected);
+        }
+
+        [Test]
+        public void Rook_move_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "Rd1", "Rd1!", "Rd1!!", "Rd1?", "Rd1??", "Rd1!?", "Rd1?!",
+                "Rd1+", "Rd1+!", "Rd1+!!", "Rd1+?", "Rd1+??", "Rd1+!?", "Rd1+?!",
+                "Rd1++", "Rd1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookMoves, expected);
+        }
+
+        [Test]
+        public void Rook_move_from_file_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "Rad1", "Rad1!", "Rad1!!", "Rad1?", "Rad1??", "Rad1!?", "Rad1?!",
+                "Rad1+", "Rad1+!", "Rad1+!!", "Rad1+?", "Rad1+??", "Rad1+!?", "Rad1+?!",
+                "Rad1++", "Rad1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookMovesFromFile, expected);
+        }
+
+        [Test]
+        public void Rook_move_from_rank_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "R5d1", "R5d1!", "R5d1!!", "R5d1?", "R5d1??", "R5d1!?", "R5d1?!",
+                "R5d1+", "R5d1+!", "R5d1+!!", "R5d1+?", "R5d1+??", "R5d1+!?", "R5d1+?!",
+                "R5d1++", "R5d1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookMovesFromRank, expected);
+        }
+
+        [Test]
+        public void Rook_captures_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "Rxd1", "Rxd1!", "Rxd1!!", "Rxd1?", "Rxd1??", "Rxd1!?", "Rxd1?!",
+                "Rxd1+", "Rxd1+!", "Rxd1+!!", "Rxd1+?", "Rxd1+??", "Rxd1+!?", "Rxd1+?!",
+                "Rxd1++", "Rxd1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookCaptures, expected);
+        }
+
+        [Test]
+        public void Rook_captures_from_file_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "Raxd1", "Raxd1!", "Raxd1!!", "Raxd1?", "Raxd1??", "Raxd1!?", "Raxd1?!",
+                "Raxd1+", "Raxd1+!", "Raxd1+!!", "Raxd1+?", "Raxd1+??", "Raxd1+!?", "Raxd1+?!",
+                "Raxd1++", "Raxd1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookCapturesFromFile, expected);
+        }
+
+        [Test]
+        public void Rook_captures_from_rank_test()
+        {
+            // Arrange
+            var expected = new List<string>
+            {
+                "R5xd1", "R5xd1!", "R5xd1!!", "R5xd1?", "R5xd1??", "R5xd1!?", "R5xd1?!",
+                "R5xd1+", "R5xd1+!", "R5xd1+!!", "R5xd1+?", "R5xd1+??", "R5xd1+!?", "R5xd1+?!",
+                "R5xd1++", "R5xd1#",
+            };
+
+            ActAndAssert(ChessRegex2.RookCapturesFromRank, expected);
         }
 
         private static void ActAndAssert(string pattern, List<string> expected)
--- Software64.ChessRegex/ChessRegex2.cs
+++ Software64.ChessRegex/ChessRegex2.cs
@@ -11,7 +11,7 @@
         /// "e4+", "e4+!", "e4+!!", "e4+?", "e4+??", "e4+!?", "e4+?!",
         /// "e4++", "e4#"
         /// </summary>
-        public static string PawnStandard => @"^([a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+        public static string PawnMoves => @"^([a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
 
         /// <summary>
         /// It matches the following type of moves: 
@@ -30,10 +30,59 @@
         public static string PawnCaptures => @"^([a-h]x[a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
 
         /// <summary>
+        /// It matches the following type of moves: 
         /// "dxe8=Q", "dxe8=Q!", "dxe8=Q!!", "dxe8=Q?", "dxe8=Q??", "dxe8=Q!?", "dxe8=Q?!",
         /// "dxe8=Q+", "dxe8=Q+!", "dxe8=Q+!!", "dxe8=Q+?", "dxe8=Q+??", "dxe8=Q+!?", "dxe8=Q+?!",
         /// "dxe8=Q++", "dxe8=Q#"
         /// </summary>
         public static string PawnCapturesPromotes => @"^([a-h]x[a-h][1-8]=[RNBQ](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "Rd1", "Rd1!", "Rd1!!", "Rd1?", "Rd1??", "Rd1!?", "Rd1?!",
+        /// "Rd1+", "Rd1+!", "Rd1+!!", "Rd1+?", "Rd1+??", "Rd1+!?", "Rd1+?!",
+        /// "Rd1++", "Rd1#"
+        /// </summary>
+        public static string RookMoves => @"^(R[a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "Rad1", "Rad1!", "Rad1!!", "Rad1?", "Rad1??", "Rad1!?", "Rad1?!",
+        /// "Rad1+", "Rad1+!", "Rad1+!!", "Rad1+?", "Rad1+??", "Rad1+!?", "Rad1+?!",
+        /// "Rad1++", "Rad1#"
+        /// </summary>
+        public static string RookMovesFromFile => @"^(R[a-h][a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "R5d1", "R5d1!", "R5d1!!", "R5d1?", "R5d1??", "R5d1!?", "R5d1?!",
+        /// "R5d1+", "R5d1+!", "R5d1+!!", "R5d1+?", "R5d1+??", "R5d1+!?", "R5d1+?!",
+        /// "R5d1++", "R5d1#",
+        /// </summary>
+        public static string RookMovesFromRank => @"^(R[1-8][a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "Rxd1", "Rxd1!", "Rxd1!!", "Rxd1?", "Rxd1??", "Rxd1!?", "Rxd1?!",
+        /// "Rxd1+", "Rxd1+!", "Rxd1+!!", "Rxd1+?", "Rxd1+??", "Rxd1+!?", "Rxd1+?!",
+        /// "Rxd1++", "Rxd1#",
+        /// </summary>
+        public static string RookCaptures => @"^(Rx[a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "Raxd1", "Raxd1!", "Raxd1!!", "Raxd1?", "Raxd1??", "Raxd1!?", "Raxd1?!",
+        /// "Raxd1+", "Raxd1+!", "Raxd1+!!", "Raxd1+?", "Raxd1+??", "Raxd1+!?", "Raxd1+?!",
+        /// "Raxd1++", "Raxd1#",
+        /// </summary>
+        public static string RookCapturesFromFile => @"^(R[a-h]x[a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
+
+        /// <summary>
+        /// It matches the following type of moves: 
+        /// "Raxd1", "Raxd1!", "Raxd1!!", "Raxd1?", "Raxd1??", "Raxd1!?", "Raxd1?!",
+        /// "Raxd1+", "Raxd1+!", "Raxd1+!!", "Raxd1+?", "Raxd1+??", "Raxd1+!?", "Raxd1+?!",
+        /// "Raxd1++", "Raxd1#",
+        /// </summary>
+        public static string RookCapturesFromRank => @"^(R[1-8]x[a-h][1-8](\+|\++|\#)?([\!\?]{1,2})?)$";
     }
 }
